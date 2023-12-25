ac26986ef330d812246823b27bddf4f26e9fdc6b
busraakcay <0busraakcay@gmail.com>
2023-12-25 09:51:09 +0300

    apollo ex

--- src/stacks/AppStack.js
+++ src/stacks/AppStack.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { screens } from '../constants/screens';
 import { Detail, Home } from '../screens';
-
+import useQuery from '../hooks/useQuery';
+import { moviesQuery } from '../graphql/moviesQuery';
 
 const Stack = createNativeStackNavigator();
 const AppStack = () => {
+
+  const { data, loading, error, sendQuery } = useQuery({
+    model: 'movies',
+    query: moviesQuery,
+  });
+
+  useEffect(() => {
+    sendQuery();
+  }, []);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error!</Text>;
   return (
     <Stack.Navigator initialRouteName={screens.Home}>
       <Stack.Screen name={screens.Home} component={Home} />
